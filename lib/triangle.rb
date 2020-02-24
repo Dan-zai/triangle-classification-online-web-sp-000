d28023f1bd72a39e76ee986f8182d37751f3919b
Daniel Cowpertwait <danielcowpertwait@gmail.com>
2020-02-24 06:39:41 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,17 +9,22 @@ class Triangle
   end
   
   def kind
+    
     if a == 0 || b == 0 || c == 0
       raise Triangle_error
-    elsif
+      
     elsif @a + @b <= @c || @a + @c <= @b || @b + @c <= @a
-    
+      raise Triangle_error
+      
     elsif a == b && b == c
     :equilateral
+    
     elsif a == b || b == c || c == a
     :isosceles
+    
     else
     :scalene
+    
   end 
 end
 end 
