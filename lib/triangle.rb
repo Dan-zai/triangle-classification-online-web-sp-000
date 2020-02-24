0e2c8b39f24692c43ca2dbb7338e34d05ada1b95
Daniel Cowpertwait <danielcowpertwait@gmail.com>
2020-02-24 06:38:48 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,7 @@ class Triangle
     if (a == 0 || b == 0 || c == 0 )
       raise Triangle_error
     elsif
-    
+    elsif @a + @b <= @c || @a + @c <= @b || @b + @c <= @a
     elsif (a == b && b == c)
     :equilateral
     elsif (a == b || b == c || c == a)
